44ca4d6a3036a08305ce59dd629568a40a9d4d15
Har01d <power0721@gmail.com>
2026-09-13 14:49:00 +0800

    fix(driver): fix share download link

--- pkg/driver/consts.go
+++ pkg/driver/consts.go
@@ -3,7 +3,7 @@ package driver
 const (
 	UADefault    = "Mozilla/5.0"
 	UADefalut    = "Mozilla/5.0" // UADefalut is deprecated: use UADefault instead. This constant exists for backward compatibility
-	UA115Browser = "Mozilla/5.0 115Browser/27.0.5.7"
+	UA115Browser = "Mozilla/5.0 115Browser/35.17.0"
 	UA115Disk    = "Mozilla/5.0 115disk/30.1.0"
 	UA115Desktop = "Mozilla/5.0 115Desktop/2.0.3.6"
 	UAIosApp     = "Mozilla/5.0; Darwin/10.0; UDown/30.1.0"
